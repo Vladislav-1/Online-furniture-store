31f706353540d4c985df70bfdb210a84bec63535
Vladislav-1 <44432412+Vladislav-1@users.noreply.github.com>
2025-03-19 21:15:00 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,125 @@
+function show_icons_0_5() {
+    document.getElementById('main__card-TATRAN-icon-left').style = "opacity: 0.5;";
+    document.getElementById('main__card-TATRAN-icon-right').style = "opacity: 0.5;";
+}
+
+function show_icons_1_0_left() {
+    document.getElementById('main__card-TATRAN-icon-left').style = "opacity: 1.0;";
+}
+
+function hide_icons_left() {
+    document.getElementById('main__card-TATRAN-icon-left').style = "opacity: 0.5;";
+}
+
+function show_icons_1_0_right() {
+    document.getElementById('main__card-TATRAN-icon-right').style = "opacity: 1.0;";
+}
+
+function hide_icons_right() {
+    document.getElementById('main__card-TATRAN-icon-right').style = "opacity: 0.5;";
+}
+
+function hide_icons() {
+    document.getElementById('main__card-TATRAN-icon-left').style = "opacity: 0.0;";
+    document.getElementById('main__card-TATRAN-icon-right').style = "opacity: 0.0;";
+}
+
+function show_rectangle_1() {
+    document.getElementById('icon-cart').style = "opacity: 0.5;";
+    document.getElementById('icon-love').style = "opacity: 0.5;";
+}
+
+function show_show_icon-cart() {
+    document.getElementById('icon-cart').style = "opacity: 1.0;";
+}
+
+function hiden_icon-cart() {
+    document.getElementById('icon-cart').style = "opacity: 0.5;";
+}
+
+function show_show_icon-love() {
+    document.getElementById('icon-love').style = "opacity: 1.0;";
+}
+
+function hiden_icon-love() {
+    document.getElementById('icon-love').style = "opacity: 0.5;";
+}
+
+function hiden_rectangle_1() {
+    document.getElementById('icon-cart').style = "opacity: 0.0;";
+    document.getElementById('icon-love').style = "opacity: 0.0;";
+}
+
+/* ----------------------------- */
+
+function show_rectangle_2() {
+    document.getElementById('rectangle_2_1').style = "opacity: 0.5;";
+    document.getElementById('rectangle_2_2').style = "opacity: 0.5;";
+}
+
+function show_show_rectangle_2_1() {
+    document.getElementById('rectangle_2_1').style = "opacity: 1.0;";
+}
+
+function hiden_rectangle_2_1() {
+    document.getElementById('rectangle_2_1').style = "opacity: 0.5;";
+}
+
+function show_show_rectangle_2_2() {
+    document.getElementById('rectangle_2_2').style = "opacity: 1.0;";
+}
+
+function hiden_rectangle_2_2() {
+    document.getElementById('rectangle_2_2').style = "opacity: 0.5;";
+}
+
+function hiden_rectangle_2() {
+    document.getElementById('rectangle_2_1').style = "opacity: 0.0;";
+    document.getElementById('rectangle_2_2').style = "opacity: 0.0;";
+}
+
+/* ----------------------------- */
+
+function show_rectangle_3() {
+    document.getElementById('rectangle_3_1').style = "opacity: 0.5;";
+    document.getElementById('rectangle_3_2').style = "opacity: 0.5;";
+}
+
+function show_show_rectangle_3_1() {
+    document.getElementById('rectangle_3_1').style = "opacity: 1.0;";
+}
+
+function hiden_rectangle_3_1() {
+    document.getElementById('rectangle_3_1').style = "opacity: 0.5;";
+}
+
+function show_show_rectangle_3_2() {
+    document.getElementById('rectangle_3_2').style = "opacity: 1.0;";
+}
+
+function hiden_rectangle_3_2() {
+    document.getElementById('rectangle_3_2').style = "opacity: 0.5;";
+}
+
+function hiden_rectangle_3() {
+    document.getElementById('rectangle_3_1').style = "opacity: 0.0;";
+    document.getElementById('rectangle_3_2').style = "opacity: 0.0;";
+}
+
+/* ----------------------------- */
+
+function showFiltr() {
+    document.getElementById('main__form').style = "display: flex;";
+}
+
+function hideFiltr() {
+    document.getElementById('main__form').style = "display: none;";
+}
+
+function showFiltrMobil() {
+    document.getElementById('main__form-mobil').style = "display: block;";
+}
+
+function hideFiltrMobil() {
+    document.getElementById('main__form-mobil').style = "display: none;";
+}
